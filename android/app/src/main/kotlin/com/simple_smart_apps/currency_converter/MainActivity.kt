5d8f9757dd2059b50208c389a86cabf1c7cd2ffd
Emmanuel Pastor <ommanu13@hotmail.fr>
2021-06-02 00:15:53 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/simple_smart_apps/currency_converter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.simple_smart_apps.currency_converter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Currency Converter',
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Hello world!',
+          style: Theme.of(context).textTheme.headline4,
+        ),
+      ),
+    );
+  }
+}

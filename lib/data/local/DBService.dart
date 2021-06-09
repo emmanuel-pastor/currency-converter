4ab32c4aa08a51dcf672518e074187e849c6c9af
Emmanuel Pastor <ommanu13@hotmail.fr>
2021-06-09 18:47:39 +0200

    [ADD] Add DB Service that can provide a database instance

---
+++ lib/data/local/DBService.dart
@@ -0,0 +1,31 @@
+import 'package:async/async.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:currency_converter/data/local/StringConstants.dart';
+
+class DBService {
+  static Future<Database> getDBInstance() async {
+    final database = openDatabase(
+      join(await getDatabasesPath(), StringConstants.dbName),
+
+      onCreate: (db, version) {
+        return _createTables(db);
+      },
+      version: 1,
+    );
+    return database;
+  }
+
+  static Future<void> _createTables(Database db) async {
+    FutureGroup futureGroup = FutureGroup();
+    futureGroup.add(_createExchangeRateTable(db));
+    futureGroup.close();
+    return futureGroup.future.then((_) => null);
+  }
+
+  static Future<void> _createExchangeRateTable(Database db) async {
+    return db.execute(
+      'CREATE TABLE ${StringConstants.tableName}(${StringConstants.id} INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, ${StringConstants.from_currency_code} TEXT, ${StringConstants.from_currency_name} TEXT, ${StringConstants.to_currency_code} TEXT, ${StringConstants.to_currency_name} TEXT, ${StringConstants.exchange_rate} FLOAT)',
+    );
+  }
+}
---
+++ lib/data/local/StringConstants.dart
@@ -0,0 +1,10 @@
+class StringConstants {
+  static const dbName = 'currency_converter_database.db';
+  static const tableName = 'exchange_rate';
+  static const id = 'id';
+  static const from_currency_code = 'from_currency_code';
+  static const from_currency_name = 'from_currency_name';
+  static const to_currency_code = 'to_currency_code';
+  static const to_currency_name = 'to_currency_name';
+  static const exchange_rate = 'exchange_rate';
+}

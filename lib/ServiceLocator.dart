5d2100e6137a2b8c3761f1ec96c00891a4e3932f
Emmanuel Pastor <ommanu13@hotmail.fr>
2021-06-15 10:00:00 +0200

    [ADD] Add dependency injection with GetIt

---
+++ lib/ServiceLocator.dart
@@ -0,0 +1,17 @@
+import 'package:currency_converter/data/local/exchange-rate-dao/ExchangeRateDAO.dart';
+import 'package:currency_converter/data/remote/exchange-rate-endpoint/ExchangeRateEndpoint.dart';
+import 'package:currency_converter/domain/features/conversion/UseCases.dart';
+import 'package:currency_converter/domain/repositories/ExchangeRateRepository.dart';
+import 'package:get_it/get_it.dart';
+
+GetIt locator = GetIt.instance;
+
+void setupLocator() {
+  // ExchangeRate related dependencies
+  locator.registerLazySingleton<ExchangeRateEndpoint>(() => ExchangeRateEndpoint.defaultInstance());
+  locator.registerLazySingleton<ExchangeRateDAO>(() => ExchangeRateDAO.defaultInstance());
+  locator.registerLazySingleton<ExchangeRateRepository>(
+      () => ExchangeRateRepository(locator<ExchangeRateDAO>(), locator<ExchangeRateEndpoint>()));
+  locator.registerFactory<ConversionUseCase>(
+      () => ConversionUseCase(locator<ExchangeRateRepository>()));
+}

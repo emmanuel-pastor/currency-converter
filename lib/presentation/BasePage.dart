7a68392c8b10f024c0efba510bb89365464f5329
Emmanuel Pastor <ommanu13@hotmail.fr>
2021-06-16 11:53:29 +0200

    [ADD] Add an initial state parameter to the BasePage

--- lib/presentation/BasePage.dart
+++ lib/presentation/BasePage.dart
@@ -1,27 +1,35 @@
+import 'package:currency_converter/presentation/ViewState.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 import '../ServiceLocator.dart';
+import 'BaseScopedModel.dart';
 
-class BasePage<T extends Model> extends StatefulWidget {
+class BasePage<T extends BaseScopedModel> extends StatefulWidget {
   final ScopedModelDescendantBuilder<T> _builder;
   final Function(T)? onScopedModelReady;
+  final ViewState? _initialState;
 
-  BasePage({required ScopedModelDescendantBuilder<T> builder, this.onScopedModelReady})
-      : this._builder = builder;
+  BasePage(
+      {required ScopedModelDescendantBuilder<T> builder, this.onScopedModelReady, ViewState? initialState})
+      : this._builder = builder,
+        this._initialState = initialState;
 
   @override
   _BasePageState<T> createState() => _BasePageState<T>();
 }
 
-class _BasePageState<T extends Model> extends State<BasePage<T>> {
+class _BasePageState<T extends BaseScopedModel> extends State<BasePage<T>> {
   T _scopedModel = locator<T>();
 
   @override
   void initState() {
     if (widget.onScopedModelReady != null) {
       widget.onScopedModelReady!(_scopedModel);
+    }
+    if (widget._initialState != null) {
+      _scopedModel.setState(widget._initialState!);
     }
     super.initState();
   }
